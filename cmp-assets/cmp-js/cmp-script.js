8b2368df0ce689ecbaefe043997abd1891ea4ae1
therrpmarketing <therrp.marketing@gmail.com>
2025-04-21 14:58:30 +0530

    Update cmp-script.js

--- cmp-assets/cmp-js/cmp-script.js
+++ cmp-assets/cmp-js/cmp-script.js
@@ -1,3 +1,21 @@
+
+// Firebase Configuration - replace with your actual Firebase credentials
+const firebaseConfig = {
+    apiKey: "AIzaSyB0yG8sGI47Zdncr21avyeJauW4Z6qJU8E",
+    authDomain: "cookie-cmp-sys.firebaseapp.com",
+    databaseURL: "https://cookie-cmp-sys-default-rtdb.firebaseio.com",
+    projectId: "cookie-cmp-sys",
+    storageBucket: "cookie-cmp-sys.appspot.com",
+    messagingSenderId: "252822009652",
+    appId: "1:252822009652:web:6bfbdf28d98953ccb89761",
+    measurementId: "G-DLGR6XFPP5"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.database();  // We're using Firebase Realtime Database
+
+
 // CMP Sidebar Loader (Modular)
 document.addEventListener("DOMContentLoaded", function () {
   const sidebarContainer = document.getElementById("cmp-sidebar");
